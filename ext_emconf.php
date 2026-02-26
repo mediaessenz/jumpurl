44726c16a7e9e97abe0cd275112d01b0b4a4092b
Alexander Grein <alexander.grein@gmail.com>
2026-02-26 12:23:43 +0100

    Bump version to 9.1.0 and update TYPO3 dependency

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,10 +8,10 @@ $EM_CONF[$_EXTKEY] = [
     'state' => 'stable',
     'clearCacheOnLoad' => 1,
     'author_company' => '',
-    'version' => '9.0.0',
+    'version' => '9.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.4.0-13.4.99',
+            'typo3' => '12.4.0-14.3.99',
         ],
         'conflicts' => [],
         'suggests' => [],
